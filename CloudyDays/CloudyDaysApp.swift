5bcdae9b710e3f5926e1511996715e7c6964c1ee
nboyers <frenttv@gmail.com>
2022-06-05 12:57:15 -0400

    Initial Commit

---
+++ CloudyDays/CloudyDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudyDaysApp.swift
+//  CloudyDays
+//
+//  Created by Noah Boyers on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudyDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
