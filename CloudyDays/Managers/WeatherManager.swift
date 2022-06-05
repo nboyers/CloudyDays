b5a97b26e030ae2fe3ebe3805517c79239d229ac
nboyers <frenttv@gmail.com>
2022-06-05 19:14:14 -0400

    Added API secret key

--- CloudyDays/Managers/WeatherManager.swift
+++ CloudyDays/Managers/WeatherManager.swift
@@ -11,7 +11,10 @@ import CoreLocation
 class WeatherManager {
     func getCurrentWeather(lat: CLLocationDegrees, long: CLLocationDegrees) async throws -> ResponseBody {
         
-        guard let weatherURL = URL(string: "http://api.openweathermap.org/geo/1.0/reverse?lat={\(lat)}&lon={\(long)}&limit={limit}&appid={\("abcd")&units=imperial}") else { fatalError("Missing URL") }
+        // Removed API KEY, you will need to get your own 
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+        guard let key = apiKey, !key.isEmpty else { fatalError("API key does not exist") }
+        guard let weatherURL = URL(string: "http://api.openweathermap.org/geo/1.0/reverse?lat={\(lat)}&lon={\(long)}&limit={limit}&appid={\(key)&units=imperial}") else { fatalError("Missing URL") }
         
         
         
